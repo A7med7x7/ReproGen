faad3ee9dfe7d219f8f787416c940ca1825ee405
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2025-07-14 21:02:09 +0300

    adding Huggingface token #8

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -10,10 +10,15 @@ if [[ ! -f "$RCLONE_CONF" ]]; then
   exit 1
 fi
 
-# Extract values of  rclone.conf
+# Extract values of rclone.conf
 S3_ACCESS_KEY=$(grep -A5 "\[$REMOTE_NAME\]" "$RCLONE_CONF" | grep "access_key_id" | cut -d '=' -f2 | xargs)
 S3_SECRET_ACCESS_KEY=$(grep -A5 "\[$REMOTE_NAME\]" "$RCLONE_CONF" | grep "secret_access_key" | cut -d '=' -f2 | xargs)
 S3_ENDPOINT_URL=$(grep -A5 "\[$REMOTE_NAME\]" "$RCLONE_CONF" | grep "endpoint" | cut -d '=' -f2 | xargs)
+
+# Hugging Face token 
+echo -n "🔐 Enter your Hugging Face token (will not be displayed): "
+read -s HF_TOKEN
+echo
 
 # Get public IP
 HOST_IP=$(curl -s ifconfig.me)
@@ -21,13 +26,14 @@ HOST_IP=$(curl -s ifconfig.me)
 # Set constant values 
 PROJECT_NAME="fancyproject"
 
-# Creatthe e .env file
+# Creating .env file
 {
   echo "S3_ACCESS_KEY=${S3_ACCESS_KEY}"
   echo "S3_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY}"
   echo "HOST_IP=${HOST_IP}"
   echo "PROJECT_NAME=${PROJECT_NAME}"
   echo "S3_ENDPOINT_URL=${S3_ENDPOINT_URL}"
+  echo "HF_TOKEN=${HF_TOKEN}"
 } > "$ENV_FILE"
 
 echo "✅ The .env file has been generated successfully at : $ENV_FILE"
